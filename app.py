6f5cb904f86db323903cb3aa8aacaabfb5c28ca6
Rahul Kumar <chaupalrahul000@gmail.com>
2025-11-12 20:08:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -110,6 +110,18 @@ Developed By Rahul !! | Titanic Survival Prediction Using ML
     unsafe_allow_html=True,
 )
 
+st.markdown(
+    """
+    <style>
+    .stApp {
+        background-image: linear-gradient(120deg, #d4fc79 0%, #96e6a1 100%);
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+
 
 
 
